1633a81547b377619a736326ff0a2408d7759aa4
Rayane Hammoumi <rayane085@gmail.com>
2023-01-30 23:59:29 +0100

    Update header.h

--- PPD_Matrices/header.h
+++ PPD_Matrices/header.h
@@ -9,21 +9,30 @@
 #include <math.h>
 #include <ctype.h>
 #include <omp.h>
-#include <time.h>      
-#include <unistd.h> 
+#include <time.h>
+#include <unistd.h>
 
-void affiche_matrice(gsl_matrix *A);
-void affiche_vecteur(gsl_vector *v, int taille);
-gsl_spmatrix *lit_fichier_mat(char nomFichier[]);
-void calcule_valeurs_et_vecteurs_propre(gsl_matrix *matrix, gsl_vector *valeurs_propres, gsl_matrix *vecteurs_propres);
-gsl_matrix *inverse_matrix(gsl_matrix *A);
+// opérations sur vecteurs
 double produit_scalaire(gsl_vector *yk, gsl_vector *yk_suivant);
-void projection(gsl_spmatrix *A, gsl_matrix *B0, gsl_matrix *B1, gsl_matrix *Vm, gsl_vector *yk, size_t taille_sous_espace);
-gsl_matrix *multiplie_matrices(gsl_matrix *matrice1, gsl_matrix *matrice2);
-void produit_matrice_vecteur(gsl_matrix *m, gsl_vector *v, gsl_vector *resultat);
-void produit_spmatrice_vecteur(gsl_spmatrix *m, gsl_vector *v, gsl_vector *resultat);
 double calcule_norme(gsl_vector *vecteur);
-int verifie_si_precision_atteinte(gsl_spmatrix *A, gsl_vector *valeurs_propres, gsl_matrix *qi, double epsilon);
 void produit_constante_vecteur(double constante, gsl_vector *vecteur, gsl_vector *resultat);
 void soustrait_vecteur2_au_vecteur1(gsl_vector *vecteur1, gsl_vector *vecteur2, gsl_vector *resultat);
+
+// opérations sur matrices
+void produit_matrice_vecteur(gsl_matrix *m, gsl_vector *v, gsl_vector *resultat);
+void produit_spmatrice_vecteur(gsl_spmatrix *m, gsl_vector *v, gsl_vector *resultat);
+void calcule_valeurs_et_vecteurs_propre(gsl_matrix *matrix, gsl_vector *valeurs_propres, gsl_matrix *vecteurs_propres);
+gsl_matrix *inverse_matrix(gsl_matrix *A);
+gsl_matrix *multiplie_matrices(gsl_matrix *matrice1, gsl_matrix *matrice2);
 void calcule_qi(gsl_spmatrix *A, gsl_matrix *qi, gsl_matrix *vecteurs_propres, gsl_matrix *Vm, size_t taille_sous_espace);
+void projection(gsl_spmatrix *A, gsl_matrix *B0, gsl_matrix *B1, gsl_matrix *Vm, gsl_vector *yk, size_t taille_sous_espace);
+
+// affichage
+void affiche_matrice(gsl_matrix *A);
+void affiche_vecteur(gsl_vector *v, int taille);
+
+// importe une sp_matrix depuis un fichier du site matrix market
+gsl_spmatrix *lit_fichier_mat(char nomFichier[]);
+
+// renvoie 1 si l'epsilon est atteint en fin d'itération
+int verifie_si_precision_atteinte(gsl_spmatrix *A, gsl_vector *valeurs_propres, gsl_matrix *qi, double epsilon);
